a7890d510b13823dfe73c5c16a6df80a2e2df011
Brandon Huettner <brandonhuet@icloud.com>
2020-03-11 10:54:18 -0500

    Updated SuicideGuidePreventionViewContoller to Swift.

---
+++ SafetyPlan/GuideSuicidePreventionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GuideSuicidePreventionViewController.swift
+//  Safety Plan
+//
+//  Created by Brandon Huettner on 3/11/20.
+//  Copyright © 2020 MoodTools. All rights reserved.
+//
+
+import UIKit
+
+
+
+class GuideSuicidePreventionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
